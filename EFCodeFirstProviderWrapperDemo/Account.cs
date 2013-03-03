00c920a932ef480989a70d9bddc427a2a81614c3
Yahav Gindi Bar <yahavgindibar@gmail.com>
2013-03-03 16:19:08 +0200

    Added missing App.config file

---
+++ EFCodeFirstProviderWrapperDemo/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstProviderWrapperDemo
+{
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string FullName { get; set; }
+
+        [MaxLength(150)]
+        public string CompanyName { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
